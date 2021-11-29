a7631f441f5d9a13e2b5224a83a55a7d29cb29cd
Saddamaryo <93894967+Saddamaryo@users.noreply.github.com>
2021-11-29 21:09:20 +0700

    Add files via upload

---
+++ root/Main Page Backend/registerfunction.php
@@ -0,0 +1,43 @@
+<html>
+<?php
+session_start();
+include("function.php");
+include("connection.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //YANG DIPOST
+    $nama_user = $_POST['nama_user'];
+    $email_user = $_POST['email_user'];
+    $nohp_user = $_POST['nohp_user'];
+    $password_user = $_POST['password_user'];
+    $userid = random_num(20);
+
+    $check = "SELECT * FROM tabel_akun_user WHERE email_user = '$email_user'";
+    $checkres = mysqli_query($con, $check);
+    $result = mysqli_num_rows($checkres);
+
+    if ($result == 1) { ?>
+        <div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                <use xlink:href="#exclamation-triangle-fill" />
+            </svg>
+            <div class="warning">
+                <?php echo "Email Already Taken"; ?>
+            </div>
+        </div>
+<?php
+        header("refresh:2;url=login-register.php");
+    } else {
+        $query = "INSERT INTO tabel_akun_user (id_user, nama_user, email_user, nohp_user, password_user) VALUES ('$userid','$nama_user', '$email_user', '$nohp_user', '$password_user')";
+        mysqli_query($con, $query);
+        header("Location: login-register.php");
+    }
+}
+?>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="an onymous">
+</head>
+<body>
+    
+</body>
+</html>
