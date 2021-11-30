b3ceeeb7ee9635139ea587935a1b375f1dcdf525
Boban Anderson <91023615+bittersandwich@users.noreply.github.com>
2021-11-30 19:14:17 +0700

    Add files via upload

---
+++ Root/admin page backend/Sprint 3 Progress/config/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host ='localhost';
+$user = 'root';
+$pass ='';
+$db ='dbmaqnahijab';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+?>
---
+++ Root/admin page backend/Sprint 3 Progress/view/dashboard.php
@@ -0,0 +1,55 @@
+      
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 fw-bold">
+                    <h1 class="main-text fw-bold">Dashboard</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                      <div class="inner">
+                        <h3>150</h3>
+        
+                        <p>New Orders</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-bag"></i>
+                      </div>
+                      <a href="view/order.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                      <div class="inner">
+                        <h3>44</h3>
+        
+                        <p>User Registrations</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                      </div>
+                      <a href="view/account.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-lg-4 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                      <div class="inner">
+                        <h3>65</h3>
+        
+                        <p>Users Make Order</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                      </div>
+                      <a href="view/order.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+            </div>
+        </div>
+   
